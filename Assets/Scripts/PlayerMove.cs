e594efbfde56a0c3dde1a486af87f7c583c9ddbc
Sleepyey <jaeeyyun@gmail.com>
2025-09-02 14:59:16 +0900

    [Feat] Start

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed = 5f;
+    private CharacterController controller;
+
+    // Start
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3 (h, 0, v);
+        controller.Move(move * speed * Time.deltaTime);
+    }
+}
